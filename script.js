120986026607ad43e113c3ff2c471a7c8552ec90
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-03 23:22:39 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// practise set 1
+// const product = {
+//     name :"PARKER JOTTER",
+//     price :285,
+//     review:4,
+//     tag:"Deal Of The Day",
+//     sale:"Five Percentage Off",
+//     totalprice:270,
+
+// }
+// console.log(product)
+
+// practise set 2
+
+const profile ={
+    Name : "ShradhaKhapra",
+    Post: 195,
+    Followers:596+"k",
+    Following:4,
+    Tag:"Entrepreneur",
+    Bio:"Apna college | ex-microsoft,DRDO To Educate Someone is the heighet privilage",
+
+}
+console.log(profile)
