7f7d1f838008126ac8f92f4853c0650f010680be
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-07 11:27:35 +0530

    initial commit

--- script.js
+++ script.js
@@ -18,20 +18,50 @@
 // let to=score==100
 // console.log(to);
 
-let studentGrade=prompt("Enter The Student Grade");
+// let studentGrade=prompt("Enter The Student Grade");
 
-if(studentGrade<=100&&studentGrade>=90){
-    console.log("Grade A")
+// if(studentGrade<=100&&studentGrade>=90){
+//     console.log("Grade A")
+// }
+// else if(studentGrade<=89&&studentGrade>=70){
+//     console.log("Grade B")
+// }
+// else if(studentGrade<=69&&studentGrade>=60){
+//     console.log("Grade C")
+// }
+// else if(studentGrade<=59&&studentGrade>=50){
+//     console.log("Grade D")
+// }
+// else{
+//     console.log("Student Is fail");
+// }
+
+let arr=[1,2,3,4,true,"five"];
+console.log(arr);
+for(let i in arr){
+console.log(arr[i]);
 }
-else if(studentGrade<=89&&studentGrade>=70){
-    console.log("Grade B")
+
+let student={
+name:"ayush",
+grade:"B",
+civicSence:0,
 }
-else if(studentGrade<=69&&studentGrade>=60){
-    console.log("Grade C")
+console.log(student.name);
+
+console.log({}=={});
+// function sum(){
+//     let a=prompt("enter the value a");
+//     let b=prompt("enter the value b");
+//     let finalSum=a+b;
+//     console.log(finalSum,typeof(a));
+// }
+// sum();
+
+let a=5;
+function sum(){
+    let b=10;
+    console.log(b);
 }
-else if(studentGrade<=59&&studentGrade>=50){
-    console.log("Grade D")
-}
-else{
-    console.log("Student Is fail");
-}
+sum();
+console.log(a);
