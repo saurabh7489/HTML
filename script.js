9d7aafee0003e52f061f1e21273508fbe3f8e132
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-15 12:44:42 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,201 +1,86 @@
+let input = document.querySelector('input');
+let btn = document.querySelector('button');
+let list = document.getElementById('list');
+btn.addEventListener("click",()=>{
+    let city=input.value;
+    let apiK='9f6290d6cda9a36a63755fadee71f83d'
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK}`).then((data)=>{
+        return data.json()
 
-document.querySelector("body").style.backgroundColor="gray";
-document.querySelector("h1").style.textAlign="center";
+    }).then((info)=>{
+        console.log(info);
+       let time= new Date().toLocaleTimeString()
+       console.log(time);
+       
+        
 
-// console.log("hello");
-// console.log(5+5);
-// console.log(5+"5");
-// console.log("*",5==="5");
-// console.log(5==5);
-// console.log(5!=5);
-// console.log(typeof(typeof(5)));
-// console.log(typeof({}));
-
-// if(undefined){
-//     console.log(true)
-// }
-// else{
-//     console.log(false);
-// }
-
-// let score = 100 
-// let to=score==100
-// console.log(to);
-
-// let studentGrade=prompt("Enter The Student Grade");
-
-// if(studentGrade<=100&&studentGrade>=90){
-//     console.log("Grade A")
-// }
-// else if(studentGrade<=89&&studentGrade>=70){
-//     console.log("Grade B")
-// }
-// else if(studentGrade<=69&&studentGrade>=60){
-//     console.log("Grade C")
-// }
-// else if(studentGrade<=59&&studentGrade>=50){
-//     console.log("Grade D")
-// }
-// else{
-//     console.log("Student Is fail");
-// }
-
-// let arr=[1,2,3,4,true,"five"];
-// console.log(arr);
-// for(let i in arr){
-// console.log(arr[i]);
-// }
-
-// let student={
-// name:"ayush",
-// grade:"B",
-// civicSence:0,
-// }
-// console.log(student.name);
-
-// console.log({}=={});
-// function sum(){
-
-//     let a=prompt("enter the value a");
-//     let b=prompt("enter the value b");
-//     let finalSum=a+b;
-//     console.log(finalSum,typeof(a));
-// }
-// sum();
-
-// let a=5;
-// function sum(){
-//     let b=10;
-//     console.log(b);
-// }
-// sum();
-// console.log(a);
-
-// console.log("jai ho");
-// let changeColor=document.getElementById("h1").style.color="red";
-// console.log(changeColor
-// console.log(a);
-// var a=5;
-// let bodyColor=document.querySelectorAll("body").style.color="green";
-// console.log(bodyColor);
-// console.log(b);
-// let b=7;
+    })
+})
 
 
-// let arr=[1,2,3,4,5,5];
-// console.log(arr.length);//length 
-
-// arr.push(6);
-// console.log(arr,'push');
-
-// arr.pop();
-// console.log(arr,'pop');
-
-// arr.unshift(0);
-// console.log(arr,'unshift');
-
-// arr.shift()
-// console.log(arr,'shift');
-
-// console.log(arr.includes(6),'include');
-// console.log(arr.indexOf(2),"indexof");
-
-// let data=arr.find((a)=>{
-//     return a==5;
-// });
-
-// arr.forEach((a,b,c)=>{
-
-// console.log(a,"a");
-
-
-// })
-// let arr1=arr.map((a,b,c,d)=>{
-//     return a;
-// })
-// console.log(arr1,"map");
-
-// let arr2=arr.filter((a)=>{
-//     return a>2;
-// })
-// console.log(arr2,"fillter");
-
-// let sum=arr.reduce((a,b,c,d)=>{
-//     console.log(a,"a");
-//     console.log(b,"B")
+// btn.addEventListener("click",()=>{
+//     let data=input.value
     
-//     return a+b;
+//     fetch(`https://api.tvmaze.com/search/shows?q=${data}`).
+//     then((info)=>{
+//         return info.json()
 
-    
+//     }).then((val)=>{
+//        console.log(val);
+       
+//             show(val)
+//     })
+
 // })
-// console.log(sum);
 
-// arra=[1,2,3,5,53,6,3,6,345,2]
 
-// let redu=arra.filter((a)=>{
-//     return a>2;
-    
-    
+// function show(val){
+//   for(let i of val){
+//     let img=  document.createElement("img")
+//     console.log(i.show.image.original,"heheh");
+//     img.setAttribute("src",i.show.image.original)   
+//     list.append(img)
+
+//   }
+// //   img.setAttribute("src",val[0].show.image.
+// //     original)
+
+//     // list.appendChild(img)
+
+// }
+
+
+
+
+
+// function fetchData(searchText) {
+//   axios.get(`https://api.tvmaze.com/search/shows?q=${searchText}`)
+//   .then(function(response) {
+//     manipulateDOM(response.data);
 // })
-// console.log(redu);
-
-
-// document.querySelector("*").style.backgroundColor="red";
-// // console.log(sum);
-// // document.querySelector("*").style.backgroundColor="red";
-// document.querySelector("h1").style.color="brown";
-// document.querySelector("h1").style.textAlign="center";
-
-// let b=30;
-// {
-//     var a=5;
-//     let b=10;
-//     const c=40;
 // }
-// console.log(a);
-// console.log(b);
-// console.log(c);
 
-// function sum(){
-//     let a=5
+// function manipulateDOM(data) {
+//   while(list.firstChild) {
+//     list.firstChild.remove();
+//   }
+
+//   for (let d of data) {
+//     if (d.show.image) {
+//       let img = document.createElement('img');
+//       img.setAttribute('src', d.show.image.original);
+//       list.appendChild(img);
+//     }  
+//   }
 // }
-// console.log(a);
-// sum()
+
+// btn.addEventListener('click', function() {
+//   let searchText = input.value;
+//   let data = fetchData(searchText);
+// })
 
 
 
-// 'use strict'
-// function sum(){
-//     console.log(this);
-// }
-// sum();
 
-// var id =10;
-// let student ={
-//     id:1,
-//     name:"kathiRole",
-//     isPass:true,
-//     sum:function(){
-//         console.log(this.id);
-//     }
-// }
-// console.log(student.name)
-// student.sum();
 
-// var a=20;
-// {
-//     let c=30;
-//     var b=40;
-// }
-// console.log(a);
-// console.log(b);
-// console.log(c);
-let student={
-    id:2,
-    name:"unknown",
-    isPass:true,
-}
-// }
-// document.querySelector("body").style.backgroundColor="gray";
-// document.querySelector("h1").style.textAlign="center";
-
+let city="bhopal"
