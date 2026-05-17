f6599abdb1dd7eb1ad1507c733b4e6e0c86446b7
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-17 23:45:12 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let input = document.querySelector('input');
-let btn = document.querySelector('button');
-let list = document.getElementById('list');
-btn.addEventListener("click",()=>{
-    let city=input.value;
-    let apiK='9f6290d6cda9a36a63755fadee71f83d'
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK}`).then((data)=>{
-        return data.json()
+// let input = document.querySelector('input');
+// let btn = document.querySelector('button');
+// let list = document.getElementById('list');
+// btn.addEventListener("click",()=>{
+//     let city=input.value;
+//     let apiK='9f6290d6cda9a36a63755fadee71f83d'
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK}`).then((data)=>{
+//         return data.json()
 
-    }).then((info)=>{
-        console.log(info);
-       let time= new Date().toLocaleTimeString()
-       console.log(time);
+//     }).then((info)=>{
+//         console.log(info);
+//        let time= new Date().toLocaleTimeString()
+//        console.log(time);
        
         
 
-    })
-})
+//     })
+// })
 
 
 // btn.addEventListener("click",()=>{
@@ -83,4 +83,14 @@ btn.addEventListener("click",()=>{
 
 
 
-let city="bhopal"
+// let city="bhopal"
+
+let change=document.querySelector("H2");
+change.innerText=change.innerText + " from appnyDuniya!"
+console.dir(change.innerText);
+let acces=document.querySelectorAll(".box");
+acces[0].innerText="jai ho";
+acces[1].innerText="jai ho";
+acces[2].innerText="jai ho";
+
+
