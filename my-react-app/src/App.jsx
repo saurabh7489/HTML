5334344f3051dd853437784d9c77055810194cd4
saurabh7489 <saurabhpandey7489@gmail.com>
2026-06-01 11:29:27 +0530

    initial commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import'./app2.css'
 
-function App() {
-  return (
-    <div>App
+const Task = () => {
+    let [search, SetSearch] = useState("")
+    let [ApiData, SetApiData] = useState([])
+
+    console.log(search);
+
+    async function apiCall() {
+        console.log("");
+        if (!search.trim()) {
+            alert("EMPTY")
+            return;
+        }
+        let res = await fetch(`https://dummyjson.com/products/search?q=${search}`)
+        let data = await res.json()
+        console.log(data);
+        SetApiData(data.products)
+
+
+    }
 
 
 
+    useEffect(() => {
+        fetch('https://dummyjson.com/products').then((res) => {
+            return res.json()
 
-      <button>click!</button>
-      const Task=() => {}
-  let {search,setsearch}=usestate("")
-  cosole.log(search);
-  
-    </div>
-  )
+        }).then((data) => {
+            console.log(data);
+            SetApiData(data.products)
+
+
+        })
+    }, [])
+
+    function fun1(){
+        console.log("dortt");
+        
+       let sortData= [...ApiData].sort((a,b)=>{
+             return a.price-b.price
+
+        })
+        SetApiData(sortData)
+        console.log(sortData,"hello");
+        
+
+    }
+
+
+    function fun2(){
+        console.log("dortt");
+        
+       let sortData= [...ApiData].sort((a,b)=>{
+             return b.price-a.price
+
+        })
+        SetApiData(sortData)
+        console.log(sortData,"hello");
+        
+
+    }
+
+
+
+    return (
+        <div>
+            <input onChange={(e) => SetSearch(e.target.value)} />
+            <button onClick={apiCall}>click</button>
+            <button onClick={fun1}>low</button>
+            <button onClick={fun2}>high</button>
+
+
+            {
+                ApiData.map((a) => {
+                    return (<div>
+                        <img src={a.
+                            thumbnail
+                        } />
+                        <h3>{a.id}</h3>
+                        <h3>{a.price}</h3>
+
+
+                    </div>)
+                })
+            }
+        </div>
+    )
 }
 
-export default App
+export default Task
+
