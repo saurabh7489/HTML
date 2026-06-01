872ee02d1206e57119b5a2e7bde39eab0515eda0
saurabh7489 <saurabhpandey7489@gmail.com>
2026-06-01 10:12:24 +0530

    initial commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,23 +1,16 @@
-import {useState} from 'react'
-import home from './home'
-const App = () => {
-  const[count,setCount]= useState(0)
-  function fun1(){
-    setCount(count+1)
-  }
-  function fun2(){
-    setCount(count -1)
-  }
+import React from 'react'
+
+function () {
   return (
     <div>
-<h2>{count}</h2>
-<button onClick={fun1} >click+</button>
-<button onClick={fun2}>click-</button>
-<style>
- 
-</style>
+
+
+
+
+
+      
     </div>
   )
 }
 
-export default App;
+export default 
